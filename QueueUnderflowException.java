e65c7d7633529f3449c971a6b37ce78ff6c8d302
FilipSegota <70112731+FilipSegota@users.noreply.github.com>
2022-02-01 11:12:24 -0500

    Add files via upload

---
+++ QueueUnderflowException.java
@@ -0,0 +1,16 @@
+/***********************************************************************
+/* Author:      Filip Segota
+/* Class:       CSC 122, Fall 2020
+/* Assignment:  Lab 3: Stack and Queue
+/* File:        QueueUnderflowExpection -> For underflowing queues
+/***********************************************************************/
+public class QueueUnderflowException extends RuntimeException {
+    //constructors
+    public QueueUnderflowException () {
+        super();
+    }
+    
+    public QueueUnderflowException(String message) {
+        super(message);
+    }
+}
---
+++ StackOverflowException.java
@@ -0,0 +1,16 @@
+/***********************************************************************
+/* Author:      Filip Segota
+/* Class:       CSC 122, Fall 2020
+/* Assignment:  Lab 3: Stack and Queue
+/* File:        StackOverflowExpection -> For overflowing stacks
+/***********************************************************************/
+public class StackOverflowException extends RuntimeException {
+    //constructors
+    public StackOverflowException () {
+        super();
+    }
+    
+    public StackOverflowException(String message) {
+        super(message);
+    }
+}
